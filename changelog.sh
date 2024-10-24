3be221623e72cdf45e9957d95e93d62449a3a0f7
Atharva Shinde <atshinde@redhat.com>
2024-10-24 13:34:39 +0530

    fix: bug in script

--- changelog.sh
+++ changelog.sh
@@ -5,16 +5,15 @@
 NEW_TAG=$(git describe --tags --abbrev=0)
 OLD_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
 
-cat << EOF
-# Changelog
+echo "# Changelog\n"> changelog.md
 
-## [$(git describe --tags --abbrev=0)] - $(date +%d-%m-%Y)
-EOF
+echo "## [$(git describe --tags --abbrev=0)] - $(date +%d-%m-%Y)" >> changelog.md
+
 for i in "feat:Added" "bug:Fixed" "refactor:Changed" "chore:Minor Changes" "remove:Removed" "deprecate:Deprecated" "security:Security" "ci:CI"; do
     TYPE=${i%%:*}
     TITLE=${i#*:}
     if [[ -n $(git log --pretty='%h - %s (%an)' ${OLD_TAG}..${NEW_TAG} | grep -i -E "^.{10}(${TYPE}: )") ]]; then
-        echo -e "\n### ${TITLE}\n"
-        git log --pretty="%h - %s (%an)" ${OLD_TAG}..${NEW_TAG} | grep -i -E "^.{10}(${TYPE}: )" | sed "s/${TYPE}: //"
+        echo -e "\n### ${TITLE}\n" >> changelog.md
+        git log --pretty="%h - %s (%an)" ${OLD_TAG}..${NEW_TAG} | grep -i -E "^.{10}(${TYPE}: )" | sed "s/${TYPE}: //" >> changelog.md
     fi
 done
